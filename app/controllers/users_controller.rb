f443009231cc60d7a85c86bcd2d8a7866d64e875
Manish0407 <manish.prajapati0492@gmail.com>
2026-02-28 14:53:16 +0530

    Add friend page showing their expenses

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @expenses = Expense.where(paid_by_id: @user.id).order(created_at: :desc)
   end
 end
