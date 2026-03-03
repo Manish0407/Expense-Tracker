43b46cd865f56bae347388b744cd4f3cf9df7e22
Manish0407 <manish.prajapati0492@gmail.com>
2026-03-03 18:19:26 +0530

    load js file in index js

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,6 +1,11 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+
+import NestedController from "./nested_controller"
 import SettleController from "./settle_controller"
+
 eagerLoadControllersFrom("controllers", application)
+
+application.register("nested", NestedController)
 application.register("settle", SettleController)
